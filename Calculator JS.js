68f0a92a054b993f5a09334827a96709eb2ba601
Joel <74183085+Dolj0@users.noreply.github.com>
2020-11-13 19:22:07 +0000

    Add files via upload

---
+++ Calculator JS.js
@@ -0,0 +1,141 @@
+let num1 = "placeHolder";
+let num2 = "placeHolder";
+let operator ="placeHolder";
+let result ="placeHolder";
+
+function val1() {
+	if (num1 != "placeHolder") {
+		num2 = 1;
+	} else {
+		num1 = 1;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val2() {
+	if (num1 != "placeHolder") {
+		num2 = 2;
+	} else {
+		num1 = 2;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val3() {
+	if (num1 != "placeHolder") {
+		num2 = 3;
+	} else {
+		num1 = 3;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val4() {
+	if (num1 != "placeHolder") {
+		num2 = 4;
+	} else {
+		num1 = 4;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val5() {
+	if (num1 != "placeHolder") {
+		num2 = 5;
+	} else {
+		num1 = 5;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val6() {
+	if (num1 != "placeHolder") {
+		num2 = 6;
+	} else {
+		num1 = 6;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val7() {
+	if (num1 != "placeHolder") {
+		num2 = 7;
+	} else {
+		num1 = 7;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val8() {
+	if (num1 != "placeHolder") {
+		num2 = 8;
+	} else {
+		num1 = 8;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val9() {
+	if (num1 != "placeHolder") {
+		num2 = 9;
+	} else {
+		num1 = 9;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function val0() {
+	if (num1 != "placeHolder") {
+		num2 = 0;
+	} else {
+		num1 = 0;
+	}
+	console.log(num1);
+	console.log(num2);
+}
+
+function opAdd() {
+	operator = 'operAdd';
+	console.log(operator);
+}
+
+function opMin() {
+	operator = 'operMin';
+	console.log(operator);
+}
+
+function opMul() {
+	operator = 'operMul';
+	console.log(operator);
+}
+
+function opDiv() {
+	operator = 'operDiv';
+	console.log(operator);
+}
+
+function equals() {
+
+	if (operator = 'operAdd') {
+		result = (num1 + num2);
+	}	
+	else if (operator = 'operMin') {
+		result = (num1 - num2);
+	}
+	else if (operator = 'operMul') {
+		result = (num1 * num2);
+	}
+	else if (operator = 'operDiv'){
+		result = (num1 / num2);
+	}
+	console.log(result);
+}
